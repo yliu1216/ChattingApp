7954dbc9fbea06070d8a1161072963e26a1ea4c9
liujia1 <lyw1532508619@outlook.com>
2023-07-02 16:57:30 -0400

    second commit

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: ['http://192.168.56.1:3000', 'http://localhost:3000'],
+    origin: ['http://192.168.56.1:3000', 'http://localhost:3000', 'http://localhost:3001'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Credentials'],
 }));
 
